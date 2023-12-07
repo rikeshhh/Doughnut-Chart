bc11846dadd8c7084c920347a3c65c07a56749e1
rikesh <70895896+rikeshhh@users.noreply.github.com>
2023-12-07 23:50:49 +0545

    border left color for each div

--- dashboard/src/Data.js
+++ dashboard/src/Data.js
@@ -4,7 +4,7 @@ const Data = [
       "title": "DIY",
       "percentage": 8.5,
       "amount": 12523,
-      "borderLeft":"border-l-4 border-[#121212]"
+      "borderLeft":"border-l-4 border-[#B22222]"
 
     },
     {
@@ -12,21 +12,21 @@ const Data = [
       "title": "PIPE",
       "percentage": 8.5,
       "amount": 30122,
-      "borderLeft":"border-l-4 border-[#121212]"
+      "borderLeft":"border-l-4 border-[#0A51A4]"
     },
     {
       "id": 3,
       "title": "AEROSPACE",
       "percentage": 8.5,
       "amount": 12523,
-      "borderLeft":"border-l-4 border-[#121212]"
+      "borderLeft":"border-l-4 border-[#A6B222]"
     },
     {
       "id": 4,
       "title": "Auto Repair",
       "percentage": 8.5,
       "amount": 30122,
-      "borderLeft":"border-l-4 border-[#121212]"
+      "borderLeft":"border-l-4 border-[#068EB9]"
 
     },
     {
@@ -34,7 +34,7 @@ const Data = [
       "title": "SHIP BUILDING",
       "percentage": 8.5,
       "amount": 12523,
-      "borderLeft":"border-l-4 border-[#121212]"
+      "borderLeft":"border-l-4 border-[#046F3C]"
 
     },
     {
@@ -42,7 +42,7 @@ const Data = [
       "title": "Medical",
       "percentage": 8.5,
       "amount": 30122,
-      "borderLeft":"border-l-4 border-[#121212]"
+      "borderLeft":"border-l-4 border-[#B24D22]"
     }
   ]
   export default Data
